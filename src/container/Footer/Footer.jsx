bbe74f5d58c019ea4f9c0242f354a7c977b7367d
AnimateReality <ronaldchittinappilly@gmail.com>
2022-10-19 23:55:50 +1300

    Footer update

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -15,11 +15,11 @@ const Footer = () => (
         <h1 className="app__footer-headtext">Contact Us</h1>
         <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
         <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
       </div>
 
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
+        <h1 style={{color: 'white'}}>Garden House</h1>
         <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
         <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
         <div className="app__footer-links_icons">
@@ -39,7 +39,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2022 Garden House. All Rights reserved.</p>
     </div>
 
   </div>
